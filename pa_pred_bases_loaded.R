5dfa3828bd86a83c16be0ba4760802d7021f4c9a
Daniel-Tucker <45470044+Daniel-Tucker@users.noreply.github.com>
2020-07-16 00:03:55 -0400

    Add files via upload

---
+++ pa_pred_bases_loaded.R
@@ -0,0 +1,25 @@
+###############################################################
+# Find the pitcher least likely to let in the winning run in a tie game 
+# against Aaron Judge in the 9th with two outs and the bases loaded
+###############################################################
+
+
+batter = sing$GetPlayers(name = 'Aaron Judge')[[1]]
+pitching_team = "Rays"
+candidate_pitchers = sing$GetPlayers(team.name=pitching_team, position = "P", active = TRUE)
+
+venue = sing$GetVenues(stadium.name = 'Yankee Stadium')[[1]]
+atmosphere = Atmosphere$new(venue = venue, temperature = 70, home_team = sing$GetTeams(name = 'Yankees')[[1]])
+state <- State$new(on_1b = TRUE, on_2b = TRUE, on_3b = TRUE, pitch_number = 0, inning = 9)
+
+
+
+matchups <- list()
+for (p in candidate_pitchers)
+{
+  matchups <- append(matchups, Matchup$new(batter = batter, pitcher = p, atmosphere = atmosphere, state = state))
+}
+
+results <- sing$GetPaSim(matchup = matchups)
+results = results[order(results$woba_exp, decreasing = TRUE), ]
+print(results)
---
+++ pa_pred_get_best_veteran.R
@@ -0,0 +1,38 @@
+###############################################################
+# Let's try to sign a veteran RH 1B or DH to platoon against the Yankees lefty pitchers 
+###############################################################
+
+pitcher_list = c('J.A. Happ', 'James Paxton', 'Jordan Montgomery')
+
+candidate_batters <- list()
+candidate_pitchers <- list()
+
+candidate_batters <- sing$GetPlayers(position = "1B", bat.side = "R", 
+                                     age.min = 32, active = TRUE)
+
+candidate_batters <- append(candidate_batters, sing$GetPlayers(position = "DH", bat.side = "R", 
+                                                               age.min = 32, active = TRUE))
+
+for (pitcher in pitcher_list)
+{
+  candidate_pitchers <- append(candidate_pitchers, sing$GetPlayers(name=pitcher))
+}
+
+
+venue <- sing$GetVenues(stadium.name = 'Yankee Stadium')[[1]]
+atmosphere <- Atmosphere$new(venue = venue, temperature = 70, home_team = sing$GetTeams(name = 'Yankees')[[1]])
+
+
+matchups <- list()
+for (b in candidate_batters) 
+{
+  for (p in candidate_pitchers)
+  {
+    matchups <- append(matchups, Matchup$new(batter = b, pitcher = p, atmosphere = atmosphere, state = State$new()))
+  }
+}
+
+
+results <- sing$GetPaSim(matchup = matchups)
+results = results[order(results$woba_exp, decreasing = TRUE), ]
+print(results)
