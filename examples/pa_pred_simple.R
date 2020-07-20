65d4b786314cca2c041955cccab777747b1bb468
Joshua Silver <joshua.silver@gmail.com>
2020-07-20 16:36:39 -0700

    Use common.R

--- examples/pa_pred_simple.R
+++ examples/pa_pred_simple.R
@@ -1,15 +1,4 @@
-library(singlearity)
-key = Sys.getenv('SINGLEARITY_API_KEY')
-if (nchar(key) == 0)
-{
-  stop("You need to set an API KEY in your environment.  Modify (or create) a .Renviron file with a line containing your API KEY, for instance:
-       SINGLEARITY_API_KEY=myveryspecialkey")
-}
-
-sing = APIsApi$new()
-sing$apiClient$apiKeys['SINGLEARITY_API_KEY'] = key
-sing$apiClient$basePath='https://beta3.singlearity.com'
-
+source(file='common.R')
 
 #list of batters
 batter_list = c('Mookie Betts')
