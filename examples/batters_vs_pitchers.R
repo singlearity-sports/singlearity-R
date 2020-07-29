bb1161d9c98309bbd1748a7f48bb9b8222b4f0be
Joshua Silver <joshua.silver@gmail.com>
2020-07-29 15:40:07 -0700

    Change team to Padres.  Only use 40 man roster.  Make font smaller

--- examples/batters_vs_pitchers.R
+++ examples/batters_vs_pitchers.R
@@ -1,6 +1,6 @@
 # This code generates heat maps of expected wOBA and K% values for every possible
 # matchup from a list of batters and pitchers. In this case, it creates graphs that
-# could be used by the Giants to select a relief pitcher, or the Dodgers to select
+# could be used by the Padres to select a relief pitcher, or the Dodgers to select
 # a pinch hitter.
 
 source(file='common.R')
@@ -26,9 +26,9 @@ candidate_batters <- append(candidate_batters, sing$GetPlayers("Will Smith", tea
 candidate_batters <- append(candidate_batters, sing$GetPlayers("Matt Beaty"))
 
 
-pitching_team = "Giants"
-candidate_pitchers = sing$GetPlayers(team.name=pitching_team, position = "P", active = TRUE)
-candidate_pitchers <- candidate_pitchers[5:17]
+pitching_team = "Padres"
+candidate_pitchers = sing$GetPlayers(team.name=pitching_team, position = "P", active = TRUE, on.40 = TRUE)
+#candidate_pitchers <- candidate_pitchers[0:17]   #shorten the list of pitchers to allow for nice visualization
 
 
 venue <- sing$GetVenues(stadium.name = 'Dodger Stadium')[[1]]
@@ -52,8 +52,8 @@ print(results)
 
 # Generate wOBA heatmap
 ggplot(results, aes(pitcher_name, batter_name, fill = woba_exp)) + geom_tile() + scale_fill_distiller(palette = "Spectral") + 
-  geom_text(aes(label = round(woba_exp, 3))) + theme(legend.position = "none") + labs(title = "wOBA by Batter and Pitcher")
+  geom_text(aes(label = round(woba_exp, 3))) + theme(legend.position = "none", axis.text=element_text(size=6)) + labs(title = "wOBA by Batter and Pitcher") 
 
 # Generate strikeout heatmap
 ggplot(results, aes(pitcher_name, batter_name, fill = so_exp)) + geom_tile() + scale_fill_distiller(palette = "Spectral") + 
-  geom_text(aes(label = round(so_exp, 3)*100)) + labs(title = "K% by Batter and Pitcher")
+  geom_text(aes(label = round(so_exp, 3)*100)) + theme(axis.text=element_text(size=6)) + labs(title = "K% by Batter and Pitcher")
