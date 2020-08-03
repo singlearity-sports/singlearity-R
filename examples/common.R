2b51f2449bb35840c3a8ff169d0b1d3eb83eb33c
Joshua Silver <joshua.silver@gmail.com>
2020-08-02 19:40:05 -0700

    Allow overriding API server using environment variable

--- examples/common.R
+++ examples/common.R
@@ -2,12 +2,18 @@ library(singlearity)
 
 #set the api key in an environment variable
 key = Sys.getenv('SINGLEARITY_API_KEY')
+server = Sys.getenv('SINGLEARITY_API_SERVER')
 if (nchar(key) == 0)
 {
   stop("You need to set an API KEY in your environment.  Modify (or create) a .Renviron file with a line containing your API KEY, for instance:
        SINGLEARITY_API_KEY=myveryspecialkey")
 }
 
-sing = APIsApi$new()
+if (nchar(server) == 0)
+{
+    #default server
+    server = 'https://api.singlearity.com'
+}
+sing <- APIsApi$new()
 sing$apiClient$apiKeys['SINGLEARITY_API_KEY'] = key
-sing$apiClient$basePath='https://api.singlearity.com'
+sing$apiClient$basePath <- server
