b24e04dbf00e1d63a1e5767d2d682a3825106b55
Tate Huffman <thuffman@college.harvard.edu>
2020-08-24 20:34:46 -0400

    Outlining tests to write

--- tests/testthat/test-singlearity-r.R
+++ tests/testthat/test-singlearity-r.R
@@ -1,1 +1,7 @@
-context("singlearity-R")
+context("Singlearity")
+
+# Head-to-head tests
+# Ensuring that outcome likelihoods are feasible
+
+# Game state tests
+# Testing that impossible outcomes (DPs, SFs, etc.) are represented correctly
