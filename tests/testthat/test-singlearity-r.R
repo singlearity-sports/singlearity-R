78d47f2d311b038b5d7a99e4945401ec41e728c5
Tate Huffman <thuffman@college.harvard.edu>
2020-08-24 20:56:25 -0400

    Updating code source for first test

--- tests/testthat/test-singlearity-r.R
+++ tests/testthat/test-singlearity-r.R
@@ -3,5 +3,14 @@ context("Singlearity")
 # Head-to-head tests
 # Ensuring that outcome likelihoods are feasible
 
+test_that("hth_results", {
+  
+  # Using simple start-of-game matchup from pa_pred_simple.R
+  # Mookie Betts vs. Chris Paddack, leading off the bottom of the 1st
+  
+  source('../../examples/pa_pred_simple.R')
+  
+})
+
 # Game state tests
 # Testing that impossible outcomes (DPs, SFs, etc.) are represented correctly
