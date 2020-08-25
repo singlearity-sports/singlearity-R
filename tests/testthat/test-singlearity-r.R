5aaad919649bbae4fba520b9da82023e98a1fc7a
Tate Huffman <thuffman@college.harvard.edu>
2020-08-24 21:43:16 -0400

    More test probabilities expected to be zero

--- tests/testthat/test-singlearity-r.R
+++ tests/testthat/test-singlearity-r.R
@@ -12,13 +12,45 @@ test_that("hth_results", {
   
   # Testing to make sure different events have zero probability:
   
-  # double plays
+  # double plays, overall
   
   expect_equal(results$dp_exp, 0)
   
   # fielder's choice
   
   expect_equal(results$fc_exp, 0)
+  
+  # ** what is "fc_o_exp"? **
+  
+  expect_equal(results$fc_o_exp, 0)
+  
+  # ** what is "fo_exp"? **
+  
+  expect_equal(results$fo, 0)
+  
+  # double plays, ground balls
+  
+  expect_equal(results$gdp_exp, 0)
+  
+  # sacrifice flies, overall
+  
+  expect_equal(results$sf_exp, 0)
+  
+  # sacrifice fly double plays
+  
+  expect_equal(results$sf_dp_exp, 0)
+  
+  # sacrifice hits
+  
+  expect_equal(results$sh_exp, 0)
+  
+  # strikeout double plays
+  
+  expect_equal(results$so_dp_exp, 0)
+  
+  # triple plays
+  
+  expect_equal(results$tp_exp, 0)
   
 })
 
