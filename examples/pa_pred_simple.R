bb2fb966cc62d93b89728cfb8e8c05e57b086a87
Tate Huffman <thuffman@college.harvard.edu>
2020-08-30 15:35:37 -0400

    Making PA prediction into a function

--- examples/pa_pred_simple.R
+++ examples/pa_pred_simple.R
@@ -4,6 +4,41 @@
 # source('~/Desktop/Everything/Singlearity/examples/common.R')
 # source(file='common.R')
 
+pa_pred_simple <- function(batters, pitchers, stadium, temp,
+                           state = State$new()) {
+  
+  #initialize empty lists
+  candidate_batters <- list()
+  candidate_pitchers <- list()
+  
+  for (batter in batters)
+  {
+    candidate_batters <- append(candidate_batters, sing$GetPlayers(name=batter))
+  }
+  
+  for (pitcher in pitchers)
+  {
+    candidate_pitchers <- append(candidate_pitchers, sing$GetPlayers(name=pitcher))
+  }
+  
+  venue <- sing$GetVenues(stadium.name = 'Dodger Stadium')[[1]]
+  atmosphere <- Atmosphere$new(venue = venue, temperature = 70, home_team = sing$GetTeams(name = 'Dodgers')[[1]])
+  
+  matchups <- list()
+  for (b in candidate_batters) 
+  {
+    for (p in candidate_pitchers)
+    {
+      matchups <- append(matchups, Matchup$new(batter = b, pitcher = p, atmosphere = atmosphere, state = State$new()))
+    }
+  }
+  
+  results <- sing$GetPaSim(matchup = matchups)
+  results = results[order(results$woba_exp, decreasing = TRUE), ]
+  print(results)
+  
+}
+
 #list of batters
 batter_list = c('Mookie Betts')
 
@@ -39,4 +74,5 @@ for (b in candidate_batters)
 results <- sing$GetPaSim(matchup = matchups)
 results = results[order(results$woba_exp, decreasing = TRUE), ]
 print(results)
+
 
