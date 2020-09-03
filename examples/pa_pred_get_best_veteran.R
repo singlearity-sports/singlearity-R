64a2c78315aac0fd7ca8ac40bb08d0b6a2b26f21
Tate Huffman <thuffman@college.harvard.edu>
2020-09-03 16:49:23 -0400

    Corrections for player acquisition predictions

--- examples/pa_pred_get_best_veteran.R
+++ examples/pa_pred_get_best_veteran.R
@@ -11,7 +11,7 @@ source(file = 'common.R')
 pa_pred_get_best_veteran <- function(num_pitchers = 2,
                                      num_pos = 2,
                                      num_side = 1,
-                                     batter_pos = c("1B, DH"),
+                                     batter_pos = c("1B", "DH"),
                                      batter_side = c("L"),
                                      age_min = 32,
                                      age_max = 45,
@@ -35,20 +35,20 @@ pa_pred_get_best_veteran <- function(num_pitchers = 2,
                                                                      active = TRUE))
     }
   }
-  
+
   # adds pitchers to list
   
   for (pitcher in pitchers)
   {
     candidate_pitchers <- append(candidate_pitchers, sing$GetPlayers(name=pitcher))
   }
-  
-  
+
   venue <- sing$GetVenues(stadium.name = stadium)[[1]]
   atmosphere <- Atmosphere$new(venue = venue, 
                                temperature = temp, 
                                home_team = sing$GetTeams(name = home)[[1]])
   
+  # Adds possible matchups to list
   
   matchups <- list()
   for (b in candidate_batters) 
@@ -58,7 +58,6 @@ pa_pred_get_best_veteran <- function(num_pitchers = 2,
       matchups <- append(matchups, Matchup$new(batter = b, pitcher = p, atmosphere = atmosphere, state = State$new()))
     }
   }
-  
   
   results <- sing$GetPaSim(matchup = matchups)
   results = results[order(results$woba_exp, decreasing = TRUE), ]
@@ -73,6 +72,7 @@ pa_pred_get_best_veteran <- function(num_pitchers = 2,
 main <- function() {
   
   args <- commandArgs(trailingOnly = TRUE)
+  print(args)
   
   if (length(args) == 0) {
     return(pa_pred_get_best_veteran())
@@ -84,73 +84,89 @@ main <- function() {
   
   # Checks to make sure we're not passing in too many arguments
   # We know that the first three arguments are number of pitchers, positions, and bat sides
-  # And w/ stadium, home team, temperature, and state, there are 12 other possible args.
+  # And w/ stadium, home team, temperature, and age min/max, there are 5 other possible args.
   # So if this doesn't add up, something's gone wrong
   
-  if (length(args) > 14 + num_bat + num_p | length(args) == 1 | length(args) == 2) {
+  if (length(args) > 8 + num_p + num_pos + num_sides | length(args) == 1 |
+      length(args) == 2 | length(args) == 3) {
     return("Invalid number of arguments.")
   }
   
   # Changes the players by looping through arguments
   # These will be passed into the function
   
-  batters_vec <- c()
+  batters_pos <- c()
   
-  for (i in 1:num_bat) {
-    batters_vec <- c(batters_vec, args[2 + i])
+  for (i in 1:num_pos) {
+    batters_pos <- c(batters_pos, args[3 + i])
   }
   
-  pitchers_vec <- c()
+  batters_sides <- c()
   
-  for (j in 1:num_p) {
-    pitchers_vec <- c(pitchers_vec, args[2 + num_bat + j])
+  for (j in 1:num_sides) {
+    batters_sides <- c(batters_sides, args[3 + num_sides + j])
   }
   
-  # Runs function if only the players have changed
+  # Runs function if just the player positions and batting sides have changed
   
-  if (length(args) == 2 + num_bat + num_p) {
-    return(pa_pred_multiplayer(num_bat, num_p, batters_vec, pitchers_vec))
+  if (length(args) == 3 + num_pos + num_sides) {
+    return(pa_pred_get_best_veteran(num_p, num_pos, num_sides, 
+                                    batters_pos, batters_sides))
+  }
+  
+  # Runs function w/ changed age min/max
+  
+  min_age <- as.numeric(args[3 + num_pos + num_sides + 1])
+  max_age <- as.numeric(args[3 + num_pos + num_sides + 2])
+  
+  if (length(args) == 3 + num_pos + num_sides + 2) {
+    return(pa_pred_get_best_veteran(num_p, num_pos, num_sides,
+                                    batters_pos, batters_sides, min_age, max_age))
+  }
+  
+  # Runs function w/ new pitchers
+  
+  pitcher_vector <- c()
+  
+  for (i in 1:num_p) {
+    pitcher_vector <- c(pitcher_vector, args[3 + num_pos + num_sides + 2 + i])
+  }
+  print(pitcher_vector)
+  
+  if (length(args) == 3 + num_pos + num_sides + 2 + num_p) {
+    return(pa_pred_get_best_veteran(num_p, num_pos, num_sides,
+                                    batters_pos, batters_sides, 
+                                    min_age, max_age, pitcher_vector))
   }
   
   # Changes the stadium
   
-  stad <- args[2 + num_bat + num_p + 1]
+  stad <- args[3 + num_pos + num_sides + 2 + num_p + 1]
   
-  if (length(args) == 2 + num_bat + num_p + 1) {
-    return(pa_pred_multiplayer(num_bat, num_p, batters_vec, pitchers_vec, stad))
-  }
+  if (length(args) == 3 + num_bat + num_sides + 2 + num_p + 1) {
+    return(pa_pred_get_best_veteran(num_p, num_pos, num_sides,
+                                    batters_pos, batters_sides, 
+                                    min_age, max_age, pitcher_vector, stad))
+  } 
   
   # Changes stadium and home team
   
-  home <- args[2 + num_bat + num_p + 2]
+  home <- args[3 + num_pos + num_sides + 2 + num_p + 2]
   
-  if (length(args) == 2 + num_bat + num_p + 2) {
-    return(pa_pred_multiplayer(num_bat, num_p, batters_vec, pitchers_vec, stad, home))
+  if (length(args) == 3 + num_bat + num_sides + 2 + num_p + 2) {
+    return(pa_pred_get_best_veteran(num_p, num_pos, num_sides,
+                                    batters_pos, batters_sides, 
+                                    min_age, max_age, pitcher_vector, stad, home))
   }
   
   # Changes stadium, home team, and temperature
   
-  temperature <- as.numeric(args[2 + num_bat + num_p + 3])
+  temperature <- as.numeric(args[3 + num_pos + num_sides + 2 + num_p + 3])
   
-  if (length(args) == 2 + num_bat + num_p + 3) {
-    return(pa_pred_multiplayer(num_bat, num_p, batters_vec, pitchers_vec, 
-                               stad, home, temperature))
-  }
-  
-  # Changes everything: stadium, home team, temp., and game state
-  
-  new_state <- State$new(inning = as.numeric(args[2 + num_bat + num_p + 4]), 
-                         to = as.logical(args[2 + num_bat + num_p + 5]), 
-                         on_1b = as.logical(args[2 + num_bat + num_p + 6]), 
-                         on_2b = as.logical(args[2 + num_bat + num_p + 7]),
-                         on_3b = as.logical(args[2 + num_bat + num_p + 8]), 
-                         outs = as.numeric(args[2 + num_bat + num_p + 9]), 
-                         bat_score = as.numeric(args[2 + num_bat + num_p + 10]), 
-                         fld_score = as.numeric(args[2 + num_bat + num_p + 11]), 
-                         pitch_number = as.numeric(args[2 + num_bat + num_p + 12]))
-  
-  return(pa_pred_multiplayer(num_bat, num_p, batters_vec, pitchers_vec, 
-                             stad, home, temperature, new_state))
+  return(pa_pred_get_best_veteran(num_p, num_pos, num_sides,
+                                  batters_pos, batters_sides, 
+                                  min_age, max_age, pitcher_vector, 
+                                  stad, home, temperature))
   
 }
 
