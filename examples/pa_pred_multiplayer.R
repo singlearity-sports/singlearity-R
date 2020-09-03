dd41d19b57ffd31a94dd9e33756e34deea570539
Tate Huffman <thuffman@college.harvard.edu>
2020-09-03 13:44:46 -0400

    Updating multiplayer prediction command-line capabilities

--- examples/pa_pred_multiplayer.R
+++ examples/pa_pred_multiplayer.R
@@ -55,7 +55,7 @@ pa_pred_multiplayer <- function(num_batters = 3,
 
 # Creates function to accept command-line arguments and run prediction function
 # To run in the command line: 
-# R -f pa_pred_simple.R --args [ARGUMENTS HERE]
+# R -f pa_pred_multiplayer.R --args [ARGUMENTS HERE]
 
 main <- function() {
   
@@ -67,105 +67,79 @@ main <- function() {
     return(pa_pred_multiplayer())
   }
   
+  num_bat <- as.numeric(args[1])
+  num_p <- as.numeric(args[2])
+  
   # Checks to make sure we're not passing in too many arguments
   # We know that the first two arguments are the number of batters and pitchers
   # And w/ stadium, home team, temperature, and state, there are 9 other possible args.
+  # So if this doesn't add up, something's gone wrong
   
-  if (length(args) > 9 + args[1] + args[2]) {
+  if (length(args) > 14 + num_bat + num_p | length(args) == 1 | length(args) == 2) {
     return("Invalid number of arguments.")
   }
 
-  # Changes the stadium
+  # Changes the players by looping through arguments
+  # These will be passed into the function
   
-  stad <- args[1]
+  batters_vec <- c()
   
-  if (length(args) == 1) {
-    return(pa_pred_simple(stad))
+  for (i in 1:num_bat) {
+    batters_vec <- c(batters_vec, args[2 + i])
   }
   
-  # Changes the stadium and home team
+  pitchers_vec <- c()
   
-  home <- args[2]
+  for (j in 1:num_p) {
+    pitchers_vec <- c(pitchers_vec, args[2 + num_bat + j])
+  }
   
-  if (length(args) == 2) {
-    return(pa_pred_simple(stad, home))
+  # Runs function if only the players have changed
+  
+  if (length(args) == 2 + num_bat + num_p) {
+    return(pa_pred_multiplayer(num_bat, num_p, batters_vec, pitchers_vec))
+  }
+  
+  # Changes the stadium
+  
+  stad <- args[2 + num_bat + num_p + 1]
+  
+  if (length(args) == 2 + num_bat + num_p + 1) {
+    return(pa_pred_multiplayer(num_bat, num_p, batters_vec, pitchers_vec, stad))
+  }
+  
+  # Changes stadium and home team
+  
+  home <- args[2 + num_bat + num_p + 2]
+  
+  if (length(args) == 2 + num_bat + num_p + 2) {
+    return(pa_pred_multiplayer(num_bat, num_p, batters_vec, pitchers_vec, stad, home))
   }
   
   # Changes stadium, home team, and temperature
   
-  temperature <- as.numeric(args[3])
+  temperature <- as.numeric(args[2 + num_bat + num_p + 3])
   
-  if (length(args) == 3) {
-    return(pa_pred_simple(stad, home, temperature))
+  if (length(args) == 2 + num_bat + num_p + 3) {
+    return(pa_pred_multiplayer(num_bat, num_p, batters_vec, pitchers_vec, 
+                               stad, home, temperature))
   }
   
-  # CHanges stadium, home team, temperature, and batter
+  # Changes everything: stadium, home team, temp., and game state
   
-  batter <- args[4]
+  new_state <- State$new(inning = as.numeric(args[2 + num_bat + num_p + 4]), 
+                         to = as.logical(args[2 + num_bat + num_p + 5]), 
+                         on_1b = as.logical(args[2 + num_bat + num_p + 6]), 
+                         on_2b = as.logical(args[2 + num_bat + num_p + 7]),
+                         on_3b = as.logical(args[2 + num_bat + num_p + 8]), 
+                         outs = as.numeric(args[2 + num_bat + num_p + 9]), 
+                         bat_score = as.numeric(args[2 + num_bat + num_p + 10]), 
+                         fld_score = as.numeric(args[2 + num_bat + num_p + 11]), 
+                         pitch_number = as.numeric(args[2 + num_bat + num_p + 12]))
   
-  if (length(args) == 4) {
-    return(pa_pred_simple(stad, home, temperature, batter))
-  }
-  
-  # Changes stadium, home team, temperature, and batter/pitcher matchup
-  
-  pitcher <- args[5]
-  
-  if (length(args) == 5) {
-    return(pa_pred_simple(stad, home, temperature, batter, pitcher))
-  }
-  
-  # Changes everything: stadium, home team, temp., matchup, and game state
-  
-  new_state <- State$new(inning = as.numeric(args[6]), 
-                         to = as.logical(args[7]), 
-                         on_1b = as.logical(args[8]), 
-                         on_2b = as.logical(args[9]),
-                         on_3b = as.logical(args[10]), 
-                         outs = as.numeric(args[11]), 
-                         bat_score = as.numeric(args[12]), 
-                         fld_score = as.numeric(args[13]), 
-                         pitch_number = as.numeric(args[14]))
-  
-  return(pa_pred_simple(stad, home, temperature, batter, pitcher, new_state))
+  return(pa_pred_multiplayer(num_bat, num_p, batters_vec, pitchers_vec, 
+                             stad, home, temperature, new_state))
   
 }
 
 main()
-
-
-#list of batters
-batter_list = c('Mookie Betts', 'Justin Turner', 'Max Muncy', 'Cody Bellinger')
-
-#list of pitchers
-pitcher_list = c('Chris Paddack', 'Emilio Pagan')
-
-#initialize empty lists
-candidate_batters <- list()
-candidate_pitchers <- list()
-
-for (batter in batter_list)
-{
-  candidate_batters <- append(candidate_batters, sing$GetPlayers(name=batter))
-}
-
-for (pitcher in pitcher_list)
-{
-  candidate_pitchers <- append(candidate_pitchers, sing$GetPlayers(name=pitcher))
-}
-
-venue <- sing$GetVenues(stadium.name = 'Dodger Stadium')[[1]]
-atmosphere <- Atmosphere$new(venue = venue, temperature = 70, home_team = sing$GetTeams(name = 'Dodgers')[[1]])
-
-matchups <- list()
-for (b in candidate_batters) 
-{
-  for (p in candidate_pitchers)
-  {
-    matchups <- append(matchups, Matchup$new(batter = b, pitcher = p, atmosphere = atmosphere, state = State$new()))
-  }
-}
-
-results <- sing$GetPaSim(matchup = matchups)
-results = results[order(results$woba_exp, decreasing = TRUE), ]
-print(results)
