d2dca650a674e3b1e172f8bc9830c113d1514a03
Joshua Silver <joshua.silver@gmail.com>
2020-09-16 20:11:14 -0700

    Add command line utility.  Simplified cmd_pa_pred.R

--- examples/pa_pred_simple.R
+++ examples/pa_pred_simple.R
@@ -1,38 +1,73 @@
+# comment out file path when running test files
+
 source(file='common.R')
 
-#list of batters
-batter_list = c('Mookie Betts')
+# turning this into a function, with a default matchup
 
-#list of pitchers
-pitcher_list = c('Chris Paddack')
-
-#initialize empty lists
-candidate_batters <- list()
-candidate_pitchers <- list()
-
-for (batter in batter_list)
-{
-  candidate_batters <- append(candidate_batters, sing$GetPlayers(name=batter))
-}
-
-for (pitcher in pitcher_list)
-{
-  candidate_pitchers <- append(candidate_pitchers, sing$GetPlayers(name=pitcher))
-}
-
-venue <- sing$GetVenues(stadium.name = 'Dodger Stadium')[[1]]
-atmosphere <- Atmosphere$new(venue = venue, temperature = 70, home_team = sing$GetTeams(name = 'Dodgers')[[1]])
-
-matchups <- list()
-for (b in candidate_batters) 
-{
-  for (p in candidate_pitchers)
+pa_pred_simple <- function(batters = sing$GetPlayers(name="Mookie Betts"),
+                           pitchers = sing$GetPlayers(name="Mike Cleveringer"),
+                           state =  State$new(), 
+                           atmosphere = Atmosphere$new(venue = sing$GetVenues(stadium.name = 'Dodger Stadium')[[1]], temperature = 70, home_team = sing$GetTeams(name = 'Dodgers')[[1]])
+                          ) {
+  
+  #initialize empty lists
+  candidate_batters <- list()
+  candidate_pitchers <- list()
+  matchups <- list()
+  
+  for (b in batters) 
   {
-    matchups <- append(matchups, Matchup$new(batter = b, pitcher = p, atmosphere = atmosphere, state = State$new()))
+    for (p in pitchers)
+    {
+      matchups <- append(matchups, Matchup$new(batter = b, pitcher = p, atmosphere = atmosphere, state = state))
+    }
   }
+  
+  results <- sing$GetPaSim(matchup = matchups)
+  results = results[order(results$woba_exp, decreasing = TRUE), ]
+  return(results)
 }
 
-results <- sing$GetPaSim(matchup = matchups)
-results = results[order(results$woba_exp, decreasing = TRUE), ]
-print(results)
+# Creates function to accept command-line arguments and run prediction function
+# To run in the command line: 
+# R -f pa_pred_simple.R --args [ARGUMENTS HERE]
+
+if (sys.nframe() == 0)
+{ #main function if run as stand-alone
+  batters_list = c('Mookie Betts', 'Cody Bellinger')
+  pitchers_list = c('Mike Clevinger', 'Chris Paddack')
+  candidate_batters <- list()
+  candidate_pitchers <- list()
+  for (batter in batters_list)
+  {
+    candidate_batters <- append(candidate_batters, sing$GetPlayers(name=trimws(batter)))
+  }
+  
+  for (pitcher in pitchers_list)
+  {
+    candidate_pitchers <- append(candidate_pitchers, sing$GetPlayers(name=trimws(pitcher)))
+  }
+  
+  
+  state <- State$new(inning = 4, 
+                         top = FALSE,   #set top = FALSE for bottom of inning
+                         on_1b = TRUE, 
+                         on_2b = FALSE,
+                         on_3b = TRUE, 
+                         outs = 1, 
+                         bat_score = 4,
+                         fld_score = 3,
+                         pitch_number = 55)
+  venue <- sing$GetVenues(stadium.name = 'Dodger Stadium')[[1]]
+  atmosphere <- Atmosphere$new(venue = venue, temperature = 70, home_team = sing$GetTeams(name = 'Dodgers')[[1]])
+   
+ 
+  results = pa_pred_simple(batters = candidate_batters,
+                              pitchers = candidate_pitchers,
+                              state = state,
+                              atmosphere = atmosphere)
+  print(results)
+  
+}
+
 
