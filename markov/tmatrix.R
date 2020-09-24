21f5f027e6d5809490891ae58dc8e3d28c6aac7c
Tate Huffman <thuffman@college.harvard.edu>
2020-09-24 16:14:29 -0400

    Inputs for standard transition matrix function

---
+++ markov/tmatrix.R
@@ -0,0 +1,30 @@
+# Function (in separate file) to get transition matrix
+# Standard function (passing in values explicitly)
+# Inputs:
+# 21 different event probabilities
+
+source("examples/common.R")
+
+tmatrix_std <- function(bb_exp = .092,
+                        ci_exp = ,
+                        double_exp = ,
+                        dp_exp = ,
+                        e_exp = ,
+                        f_out_exp = ,
+                        fc_exp = ,
+                        fc_o_exp = ,
+                        fo_exp = ,
+                        gdp_exp = ,
+                        hbp_exp = ,
+                        hr_exp = ,
+                        ibb_exp = ,
+                        sf_exp = ,
+                        sf_dp_exp = ,
+                        sh_exp = ,
+                        single_exp = ,
+                        so_exp = .234,
+                        so_dp_exp = ,
+                        tp_exp = ,
+                        triple_exp = ) {
+  
+}
