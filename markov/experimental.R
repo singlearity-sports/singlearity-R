461d9c34f1f8005b4e3f085132181263222c7a81
Tate Huffman <thuffman@college.harvard.edu>
2020-10-26 16:26:17 -0400

    New PBP file and updated experimental work

--- markov/experimental.R
+++ markov/experimental.R
@@ -3,6 +3,7 @@
 library(singlearity)
 suppressPackageStartupMessages(library(tidyverse))
 suppressPackageStartupMessages(library(baseballr))
+suppressPackageStartupMessages(library(lubridate))
 
 # Assumes user is in the overarching "Singlearity" directory 
 
@@ -190,7 +191,42 @@ pbp_2016_first <- read_csv("markov/data/pbp_2016_first.csv") %>%
          on_1b = na_if(on_1b, "null")) %>% 
   run_expectancy_code
 
-pbp_first <- bind_rows(pbp_2020_first, pbp_2019_first, pbp_2018_first,
+pbp_2015_first <- read_csv("markov/data/pbp_2015_first.csv") %>% 
+  mutate(release_speed = as.numeric(release_speed),
+         release_pos_x = as.numeric(release_pos_x),
+         release_pos_y = as.numeric(release_pos_y),
+         release_pos_z = as.numeric(release_pos_z),
+         zone = as.numeric(zone),
+         pfx_x = as.numeric(pfx_x),
+         pfx_z = as.numeric(pfx_z),
+         plate_x = as.numeric(plate_x),
+         plate_z = as.numeric(plate_z),
+         vx0 = as.numeric(vx0),
+         vy0 = as.numeric(vy0),
+         vz0 = as.numeric(vz0),
+         ax = as.numeric(ax),
+         ay = as.numeric(ay),
+         az = as.numeric(az),
+         sz_top = as.numeric(sz_top),
+         sz_bot = as.numeric(sz_bot),
+         effective_speed = as.numeric(effective_speed),
+         release_spin_rate = as.numeric(release_spin_rate),
+         release_extension = as.numeric(release_extension),
+         fielder_2 = as.numeric(fielder_2),
+         fielder_3 = as.numeric(fielder_3),
+         fielder_4 = as.numeric(fielder_4),
+         fielder_5 = as.numeric(fielder_5),
+         fielder_6 = as.numeric(fielder_6),
+         fielder_7 = as.numeric(fielder_7),
+         fielder_8 = as.numeric(fielder_8),
+         fielder_9 = as.numeric(fielder_9)) %>% 
+  select(-fielder_2_1) %>% 
+  mutate(on_3b = na_if(on_3b, "null"),
+         on_2b = na_if(on_2b, "null"),
+         on_1b = na_if(on_1b, "null")) %>% 
+  run_expectancy_code
+
+pbp_first <- bind_rows(pbp_2020_first, pbp_2019_first, pbp_2018_first, 
                        pbp_2017_first, pbp_2016_first) %>% 
   arrange(desc(game_date))
 
@@ -216,11 +252,16 @@ season_re24 <- function(pbp) {
 
 # Uses above function to get 1st inning RE24 tables for each year
 
-re24_2020_first <- season_re24(pbp_2020_first)
 re24_2019_first <- season_re24(pbp_2019_first)
 re24_2018_first <- season_re24(pbp_2018_first)
 re24_2017_first <- season_re24(pbp_2017_first)
 re24_2016_first <- season_re24(pbp_2016_first)
+re24_2015_first <- season_re24(pbp_2015_first)
+
+# Error tracker (first elmt. = Singlearity, second elmt. = standard)
+
+error <- c(0, 0)
+num_pa <- 0
 
 # Function to get difference between Singlearity predictions and runs scored
 
@@ -294,6 +335,104 @@ inning_diff <- function(game_id) {
   info_away[[6]] <- game_info %>% 
     select(game_date) %>% 
     pull()
+  
+  # Gets transition matrices for away team
+  
+  tmatrices_away <- markov_matrices(standard = FALSE,
+                                    state = State$new(top = TRUE),
+                                    info = info_away)
+  
+  # Does same as above, but for the home lineup
+  
+  info_home <- list()
+  
+  # Creates list of player names
+  
+  info_home[[1]] <- as.list(lineup_home$fullName)
+  
+  # Gets pitcher name
+  
+  info_home[[2]] <- pbp_first %>% 
+    filter(game_pk == game_id, inning_topbot == "Bot") %>% 
+    select(pitcher) %>% 
+    pull() %>% 
+    sing$GetPlayers(id = .) %>% 
+    pluck(1, "full_name")
+  
+  # Gets stadium name
+  
+  info_home[[3]] <- info_away[[3]]
+  
+  # Gets name of home team
+  
+  info_home[[4]] <- info_away[[4]]
+  
+  # Gets temperature
+  
+  info_home[[5]] <- info_away[[5]]
+  
+  # Gets date
+  
+  info_home[[6]] <- info_away[[6]]
+  
+  # Gets transition matrices for home team
+  
+  tmatrices_home <- markov_matrices(info = info_home)
+  
+  # Isolates play-by-play outcomes for each half-inning
+  
+  pbp_away <- pbp_first %>% 
+    filter(game_pk == game_id, inning_topbot == "Top")
+  
+  pbp_home <- pbp_first %>% 
+    filter(game_pk == game_id, inning_topbot == "Bot")
+  
+  for (pa in seq_len(nrow(pbp_away))) {
+    
+    # Gets index for batting order, w/ extra precaution should a team bat around
+    
+    index <- pa %% 9
+    
+    if (index == 0) {
+      
+      index <- 9
+      
+    }
+    
+    # Grabs the specific plate appearance
+    
+    ab <- pbp_away %>% 
+      slice(pa)
+    
+    # Creates state
+    
+    test_state <- State$new(top = TRUE,
+                            outs = pull(select(ab, outs_when_up)),
+                            on_1b = !is.na(pull(select(ab, on_1b))),
+                            on_2b = !is.na(pull(select(ab, on_2b))),
+                            on_3b = !is.na(pull(select(ab, on_3b))))
+    
+    # Gets Markov predictions, specifically expected runs
+    
+    runs_exp <- markov_half_inning(idx = index,
+                                   tmatrix_list = tmatrices_away,
+                                   state = test_state) %>% 
+      pluck(1)
+    
+    # Updates error for Singlearity
+    
+    error[1] <- (runs_exp - pull(select(ab, runs_to_end_inning)))^2
+    num_pa <- num_pa + 1
+    
+    # Updates error for standard pred., using previous year's RE24 table
+    
+    year_prev <- game_info %>% 
+      select(game_date) %>% 
+      pull() %>% 
+      as.Date() %>% 
+      year() - 1
+    
+  }
   
   # For each PA:
   # Gets expected runs using markov_half_inning()
