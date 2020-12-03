a4c60e0d7a0cf6e5a2b729d13f50910f5489929e
Tate Huffman <thuffman@college.harvard.edu>
2020-12-02 21:14:08 -0500

    Combining wOBA functionality to decrease runtime. Consolidating home/away calls

--- markov/experimental.R
+++ markov/experimental.R
@@ -129,6 +129,51 @@ for (game in game_ids) {
   
 }
 
+# Creates PA prediction function for use in grabbing wOBAs
+
+pa_pred_fn <- function(batters = c(605141),
+                       pitchers = c(605182),
+                       states =  list(State$new()), 
+                       atmosphere = Atmosphere$new(venue = 
+                                                     sing$GetVenues(stadium.name = 
+                                                                      'Dodger Stadium')[[1]], 
+                                                   temperature = 70, 
+                                                   home_team = sing$GetTeams(name = 
+                                                                               'Dodgers')[[1]]),
+                           date = format(Sys.Date(), "%Y-%m-%d"),
+                           predictiontype = 'ab_outcome') 
+  {
+  
+  candidate_batters <- list()
+  candidate_pitchers <- list()
+  
+  for (batter in batters)
+  {
+    candidate_batters <- append(candidate_batters, sing$GetPlayers(id=batter))
+  }
+  
+  for (pitcher in pitchers)
+  {
+    candidate_pitchers <- append(candidate_pitchers, sing$GetPlayers(id=pitcher))
+  }
+  
+  #initialize empty lists
+  matchups <- list()
+  
+  for (i in seq(length(states))) {
+    for (p in candidate_pitchers) {
+      matchups <- append(matchups, Matchup$new(batter = candidate_batters[[i]], 
+                                               pitcher = candidate_pitchers[[1]], 
+                                               atmosphere = atmosphere, 
+                                               state = states[[i]], 
+                                               date = date))
+    }
+  }
+  
+  results <- sing$GetPaSim(matchup = matchups)
+  return(results)
+}
+
 # Similar procedure as above, but now getting predictions instead
 
 result_data <- tibble(game_date = character(),
@@ -185,196 +230,131 @@ inning_diff <- function(game) {
   pbp_home <- pbp_first %>% 
     filter(game_pk == game, inning_topbot == "Bot")
   
-  for (pa in seq_len(nrow(pbp_away))) {
+  # Creating function over which to iterate to get results
+  
+  pa_iterate <- function(pbp, indiv_game_info, tmatrices, away) {
     
-    # Gets index for batting order, w/ extra precaution should a team bat around
-    
-    index <- pa %% 9
-    
-    if (index == 0) {
+    state_list <- list()
+    for (row in 1:nrow(pbp)) {
+
+      ab <- pbp %>% 
+        slice(row)
+      state_list <- append(state_list, 
+                           State$new(top = away,
+                                     outs = pull(select(ab, outs_when_up)),
+                                     on_1b = !is.na(pull(select(ab, on_1b))),
+                                     on_2b = !is.na(pull(select(ab, on_2b))),
+                                     on_3b = !is.na(pull(select(ab, on_3b)))))
+    }
+
+    if (away) {
       
-      index <- 9
+      pa_pred <- pa_pred_fn(batters = indiv_game_info$away_lineup[[1]],
+                            pitchers = indiv_game_info$pitcher_vs_away,
+                            state = state_list,
+                            atmosphere = Atmosphere$new(venue = sing$GetVenues(stadium.name = indiv_game_info$stadium)[[1]],
+                                                        temperature = indiv_game_info$temperature, 
+                                                        home_team = sing$GetTeams(name = indiv_game_info$team_home)[[1]]),
+                            date = indiv_game_info$game_date)
+    } else {
       
+      pa_pred <- pa_pred_fn(batters = indiv_game_info$home_lineup[[1]],
+                            pitchers = indiv_game_info$pitcher_vs_home,
+                            state = state_list,
+                            atmosphere = Atmosphere$new(venue = sing$GetVenues(stadium.name = indiv_game_info$stadium)[[1]],
+                                                        temperature = indiv_game_info$temperature, 
+                                                        home_team = sing$GetTeams(name = indiv_game_info$team_home)[[1]]),
+                            date = indiv_game_info$game_date)
     }
     
-    # Grabs the specific plate appearance
-    
-    ab <- pbp_away %>% 
-      slice(pa)
-    
-    # Creates state
-    
-    test_state <- State$new(top = TRUE,
-                            outs = pull(select(ab, outs_when_up)),
-                            on_1b = !is.na(pull(select(ab, on_1b))),
-                            on_2b = !is.na(pull(select(ab, on_2b))),
-                            on_3b = !is.na(pull(select(ab, on_3b))))
+    print(pa_pred)
+    for (pa in seq_len(nrow(pbp))) {
 
-    # Gets wOBA prediction
-    
-    pa_pred <- pa_pred_simple(batters = sing$GetPlayers(id = ab$batter),
-                              pitchers = sing$GetPlayers(id = indiv_game_info$pitcher_vs_away),
-                              state = test_state,
-                              atmosphere = Atmosphere$new(venue = sing$GetVenues(stadium.name = indiv_game_info$stadium)[[1]], 
-                                                          temperature = indiv_game_info$temperature, 
-                                                          home_team = sing$GetTeams(name = indiv_game_info$team_home)[[1]]),
-                              date = indiv_game_info$game_date)
+      # Gets index for batting order, w/ extra precaution should a team bat around
+      
+      index <- pa %% 9
+      
+      if (index == 0) {
+        index <- 9
+      }
+      
+      ab <- pbp %>% 
+        slice(pa)
+      
+      test_state <- State$new(top = away,
+                              outs = pull(select(ab, outs_when_up)),
+                              on_1b = !is.na(pull(select(ab, on_1b))),
+                              on_2b = !is.na(pull(select(ab, on_2b))),
+                              on_3b = !is.na(pull(select(ab, on_3b))))
 
-    woba <- pa_pred %>% 
-      select(woba_exp) %>% 
-      pull()
-    
-    batter_name <- pa_pred %>% 
-      select(batter_name) %>% 
-      pull()
-    
-    pitcher_name <- pa_pred %>% 
-      select(pitcher_name) %>% 
-      pull()
-    
-    # Gets Markov predictions, specifically expected runs
-    
-    runs_exp <- markov_half_inning(idx = index,
-                                   tmatrix_list = tmatrices_away,
-                                   state = test_state) %>% 
-      pluck(1) %>% 
-      as.numeric()
-    
-    # Gets prediction for this state using previous year's RE24
-    
-    year_prev <- indiv_game_info %>% 
-      select(game_date) %>% 
-      pull() %>% 
-      as.Date() %>% 
-      year() - 1
-    
-    re24 <- get(paste0("re24_", year_prev, "_first"))
-    
-    re_est <- re24 %>% 
-      filter(outs == test_state$outs,
-             on_1b == test_state$on_1b,
-             on_2b == test_state$on_2b,
-             on_3b == test_state$on_3b) %>% 
-      select(avg_re) %>% 
-      pull() %>% 
-      as.numeric()
+      # Gets wOBA prediction
+      
+      woba <- pa_pred %>% 
+        filter(batter == pull(select(ab, batter))) %>% 
+        select(woba_exp) %>% 
+        pull()
 
-    # Adds into tibble
+      batter_name <- pa_pred %>% 
+        filter(batter == pull(select(ab, batter))) %>%
+        select(batter_name) %>% 
+        pull()
+      
+      pitcher_name <- pa_pred %>% 
+        filter(batter == pull(select(ab, batter))) %>%
+        select(pitcher_name) %>% 
+        pull()
+      
+      # Gets Markov predictions, specifically expected runs
+      
+      runs_exp <- markov_half_inning(idx = index,
+                                     tmatrix_list = tmatrices,
+                                     state = test_state) %>% 
+        pluck(1) %>% 
+        as.numeric()
+      
+      # Gets prediction for this state using previous year's RE24
+      
+      year_prev <- indiv_game_info %>% 
+        select(game_date) %>% 
+        pull() %>% 
+        as.Date() %>% 
+        year() - 1
+      
+      re24 <- get(paste0("re24_", year_prev, "_first"))
+      
+      re_est <- re24 %>% 
+        filter(outs == test_state$outs,
+               on_1b == test_state$on_1b,
+               on_2b == test_state$on_2b,
+               on_3b == test_state$on_3b) %>% 
+        select(avg_re) %>% 
+        pull() %>% 
+        as.numeric()
+      
+      # Adds into tibble
+      
+      result_data <- result_data %>% 
+        add_row(game_date = indiv_game_info$game_date,
+                game_id = indiv_game_info$game_id,
+                batter_id = ab$batter,
+                batter_name = batter_name,
+                pitcher_id = ab$pitcher,
+                pitcher_name = pitcher_name,
+                top_bot = ab$inning_topbot,
+                start = ab$base_out_state,
+                end = ab$next_base_out_state,
+                pred_woba = woba,
+                pred_sing = runs_exp,
+                pred_re24 = re_est,
+                actual = ab$runs_to_end_inning)
+    }
     
-    result_data <- result_data %>% 
-      add_row(game_date = indiv_game_info$game_date,
-              game_id = game,
-              batter_id = ab$batter,
-              batter_name = batter_name,
-              pitcher_id = ab$pitcher,
-              pitcher_name = pitcher_name,
-              top_bot = ab$inning_topbot,
-              start = ab$base_out_state,
-              end = ab$next_base_out_state,
-              pred_woba = woba,
-              pred_sing = runs_exp,
-              pred_re24 = re_est,
-              actual = ab$runs_to_end_inning)
-
+    return(result_data)
+    
   }
   
-  for (pa in seq_len(nrow(pbp_home))) {
-    
-    # Gets index for batting order, w/ extra precaution should a team bat around
-    
-    index <- pa %% 9
-    
-    if (index == 0) {
-      
-      index <- 9
-      
-    }
-    
-    # Grabs the specific plate appearance
-    
-    ab <- pbp_home %>% 
-      slice(pa)
-    
-    # Creates state
-    
-    test_state <- State$new(top = FALSE,
-                            outs = pull(select(ab, outs_when_up)),
-                            on_1b = !is.na(pull(select(ab, on_1b))),
-                            on_2b = !is.na(pull(select(ab, on_2b))),
-                            on_3b = !is.na(pull(select(ab, on_3b))))
-    
-    # Gets wOBA prediction
-    
-    pa_pred <- pa_pred_simple(batters = sing$GetPlayers(id = ab$batter),
-                              pitchers = sing$GetPlayers(id = indiv_game_info$pitcher_vs_home),
-                              state = test_state,
-                              atmosphere = Atmosphere$new(venue = sing$GetVenues(stadium.name = indiv_game_info$stadium)[[1]], 
-                                                          temperature = indiv_game_info$temperature, 
-                                                          home_team = sing$GetTeams(name = indiv_game_info$team_home)[[1]]),
-                              date = indiv_game_info$game_date)
-
-    woba <- pa_pred %>% 
-      select(woba_exp) %>% 
-      pull()
-    
-    batter_name <- pa_pred %>% 
-      select(batter_name) %>% 
-      pull()
-    
-    pitcher_name <- pa_pred %>% 
-      select(pitcher_name) %>% 
-      pull()
-
-    # Gets Markov predictions, specifically expected runs
-    
-    runs_exp <- markov_half_inning(idx = index,
-                                   tmatrix_list = tmatrices_home,
-                                   state = test_state) %>% 
-      pluck(1) %>% 
-      as.numeric()
-
-    # Updates error for Singlearity
-    
-    # error[1] <- error[1] + (runs_exp - as.numeric(pull(select(ab, runs_to_end_inning))))^2
-    # num_pa <- num_pa + 1
-    
-    # Updates error for standard pred., using previous year's RE24 table
-    
-    year_prev <- indiv_game_info %>% 
-      select(game_date) %>% 
-      pull() %>% 
-      as.Date() %>% 
-      year() - 1
-    
-    re24 <- get(paste0("re24_", year_prev, "_first"))
-    
-    re_est <- re24 %>% 
-      filter(outs == test_state$outs,
-             on_1b == test_state$on_1b,
-             on_2b == test_state$on_2b,
-             on_3b == test_state$on_3b) %>% 
-      select(avg_re) %>% 
-      pull() %>% 
-      as.numeric()
-
-    # error[2] <- error[2] + (re_est - as.numeric(pull(select(ab, runs_to_end_inning))))^2
-    
-    result_data <- result_data %>% 
-      add_row(game_date = indiv_game_info$game_date,
-              game_id = game,
-              batter_id = ab$batter,
-              batter_name = batter_name,
-              pitcher_id = ab$pitcher,
-              pitcher_name = pitcher_name,
-              top_bot = ab$inning_topbot,
-              start = ab$base_out_state,
-              end = ab$next_base_out_state,
-              pred_woba = woba,
-              pred_sing = runs_exp,
-              pred_re24 = re_est,
-              actual = ab$runs_to_end_inning)
-    
-  }
+  result_data <- pa_iterate(pbp_away, indiv_game_info, tmatrices_away, T)
+  result_data <- pa_iterate(pbp_home, indiv_game_info, tmatrices_home, F)
   
   # For each PA:
   # Gets expected runs using markov_half_inning()
